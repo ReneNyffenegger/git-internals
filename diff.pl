02617cc44c64635a087bf5c4c38e6a9b6fd4a590
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-06-22 16:51:15 +0200

    + diff.pl

---
+++ diff.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use GitInternals;
+
+my $gi = new GitInternals ( [ 'Alice']);
+
+$gi -> exec('Alice', 'git init'                                    , no_cmp => 1);
+
+$gi -> exec('Alice', 'printf "foo\nabr\nbaz\n" > foo.txt'          , no_cmp => 1);
+$gi -> exec('Alice', 'printf "one\ntwo\nthree\n" > 123.txt'        , no_cmp => 1);
+
+$gi -> exec('Alice', 'git add .'                                   , no_cmp => 1);
+$gi -> exec('Alice', 'git commit . -m "first revision"'            , no_cmp => 1);
+
+$gi -> exec('Alice', 'sed -i s/abr/bar/ foo.txt'                   , no_cmp => 1);
+
+$gi -> exec('Alice', 'echo hello > world.txt'                      , no_cmp => 1);
+
+$gi -> exec('Alice', 'git status'                                  , no_cmp => 1);
+$gi -> exec('Alice', 'git diff --cached'                           , no_cmp => 1);
+$gi -> exec('Alice', 'git diff'                                    , no_cmp => 1);
+
+$gi -> exec('Alice', 'git add world.txt'                           , no_cmp => 1);
+
+$gi -> exec('Alice', 'git diff --cached'                           , no_cmp => 1);
+$gi -> exec('Alice', 'git diff'                                    , no_cmp => 1);
+
+
+
+$gi -> end;
