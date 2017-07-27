5d35bfd17ba99acafa0d17dd52879e5d38c9b8a4
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-27 11:28:42 +0200

    Some text_pre

--- hash-object.pl
+++ hash-object.pl
@@ -7,12 +7,22 @@ use GitInternals;
 my $gi = GitInternals -> new(['Alice']);
 
 $gi -> exec('Alice', 'git init', no_cmp => 1);
+
 $gi -> exec('Alice', "printf 'one foo\ntwo bar\n' > some-blob");
-$gi -> exec('Alice', "git hash-object -w some-blob");
-$gi -> exec('Alice', "git cat-file -p 9363c4e4606084a3c1fca83a3c535f65582ac834");
+
+$gi -> exec('Alice', "git hash-object -w some-blob",
+             text_pre => 'git hash-object with -w takes a file, computes the object id, and stores it in the .git/object directory');
+
+$gi -> exec('Alice', "git cat-file -p 9363c4e4606084a3c1fca83a3c535f65582ac834",
+             text_pre =>'Use git cat-file -p to show the content of an object');
+
 $gi -> exec('Alice', "printf 'three baz\n' >> some-blob");
+
 $gi -> exec('Alice', "git hash-object -w some-blob");
+
 $gi -> exec('Alice', "git cat-file -p 8db444301e5246a004784c9c7f39ab89725c3d2b");
-$gi -> exec('Alice', "git cat-file -t 8db444301e5246a004784c9c7f39ab89725c3d2b");
+
+$gi -> exec('Alice', "git cat-file -t 8db444301e5246a004784c9c7f39ab89725c3d2b",
+             text_pre=>"cat-file with -t shows the object type rather than the object's content");
 
 $gi -> end;
