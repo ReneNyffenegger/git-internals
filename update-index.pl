c3a9a32febd61d6ac39d71d20cddb3b06c92936d
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-27 13:35:01 +0200

    update without --add

--- update-index.pl
+++ update-index.pl
@@ -10,10 +10,10 @@ my $gi = GitInternals -> new(['Alice']);
 
 $gi -> exec('Alice', 'git init', no_cmp => 1);
 
-$gi -> exec('Alice', 'printf "foo\nbar\nbaz"   > foo-bar-baz.txt'  ,
+$gi -> exec('Alice', 'printf "foo\nbar\nbaz\n"   > foo-bar-baz.txt'  ,
        text_pre=>'First, Alice creates two files: foo-bar-baz.txt and …');
 
-$gi -> exec('Alice', 'printf "one\ntwo\nthree" > one-two-three.txt',
+$gi -> exec('Alice', 'printf "one\ntwo\nthree\n" > one-two-three.txt',
        text_pre=>'… one-two-three.txt.');
 
 $gi -> exec('Alice', 'git update-index --add foo-bar-baz.txt'      ,
@@ -30,9 +30,15 @@ $gi -> exec('Alice', 'git update-index --remove foo-bar-baz.txt'   ,
 $gi -> exec('Alice', 'rm foo-bar-baz.txt'                          ,
        text_pre=>'So, the file is removed …');
 
-
 $gi -> exec('Alice', 'git update-index --remove foo-bar-baz.txt'   ,
        text_pre=>'… and then the index is updated',
-       text_post=>'Note: the object file is still in the ./git/objects directory.');
+       text_post=>'foo-bar-baz.txt is removed from the index, however, the object file is still in the ./git/objects directory.');
+
+$gi -> exec('Alice', 'Echo "Appended line" >> one-two-three.txt',
+       text_pre => 'Modify one-two-three.txt (append a line)');
+
+$gi -> exec('Alice', 'git update-index one-two-three.txt',
+       text_pre => 'Updating the index with the newer version of one-two-three.txt (Appended line). Note: no -add option is used this time because the file is already known.',
+       text_post=> 'The index now tracks one-two-three.txt with the new object id 5325806….');
 
 $gi -> end;
