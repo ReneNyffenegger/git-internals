a58ac241062aa1bacab753f96ebc41ef3adc08d1
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-30 16:05:06 +0200

    Use --template=

--- init.pl
+++ init.pl
@@ -6,6 +6,8 @@ use GitInternals;
 
 my $gi = new GitInternals ( [ 'Alice']);
 
-$gi -> exec('Alice', 'git init');
+$gi -> exec('Alice', 'git init --template=',
+  text_pre=>'Initialize a new git repository. Use the <code>--template=</code> parameter to indicate that git should not copy (sample) hook files into the new repository.',
+  text_post=>'The content of .git/HEAD is refs/heads/master although this file does not yet exist.');
 
 $gi -> end;
